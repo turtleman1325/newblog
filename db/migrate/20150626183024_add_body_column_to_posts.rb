519848af526b3567cccbcbeac9b6757ec624083d
Art <art19ad4@gmail.com>
2015-06-26 12:53:36 -0700

    initial commit

---
+++ db/migrate/20150626183024_add_body_column_to_posts.rb
@@ -0,0 +1,5 @@
+class AddBodyColumnToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :body, :text
+  end
+end
---
+++ db/migrate/20150626194401_add_category_column_in_posts.rb
@@ -0,0 +1,5 @@
+class AddCategoryColumnInPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :category, :string
+  end
+end
